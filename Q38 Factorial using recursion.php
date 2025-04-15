76d4d2bb634c226c3eef557a6470b246f94d61c4
ARYAN-57-lab <vanikasadana2512@gmail.com>
2025-04-15 22:18:34 +0530

    Update Q38 Factorial using recursion.php

--- Q38 Factorial using recursion.php
+++ Q38 Factorial using recursion.php
@@ -8,5 +8,5 @@ return $number*factorial($number-1);
 $number=5;
 $result=factorial($number);
 echo"Factorial of $number is $result";
-echo("<br>This program is written by Tanya Aggarwal 0221BCA084");
+echo "Program by Aryan(0221BCA161)";
 ?>
